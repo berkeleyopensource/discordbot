07f1832e100a4ee4f45c7decd45e064a77da955d
feefs <feefs@berkeley.edu>
2020-05-21 00:53:36 -0700

    fixed dadjokes

--- regular_commands/dadjoke.js
+++ regular_commands/dadjoke.js
@@ -1,7 +1,8 @@
+const fetch = require('node-fetch')
 module.exports = {
     name: 'dadjoke',
     description: 'get a random dadjoke!',
-    execute (message, args) {
+    async execute (message, args) {
         j = await fetch("https://icanhazdadjoke.com/", {headers: {Accept: "text/plain"}})
         return message.channel.send(await j.text())
     }
