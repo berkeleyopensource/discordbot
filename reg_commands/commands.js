3f6ca12b75da7db14d02647a5287271d9e9d528b
Vanshaj Singhania <vanshaj@berkeley.edu>
2020-05-21 14:10:57 -0700

    stock command v1

--- reg_commands/commands.js
+++ reg_commands/commands.js
@@ -1,9 +1,11 @@
 import foo from './foo.js'
 import classtorole from './classtorole.js'
 import dadjoke from './dadjoke.js'
+import stock from './stock.js'
 
 export default [
     foo,
     classtorole,
-    dadjoke
+    dadjoke,
+    stock
 ]
---
+++ reg_commands/stock.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+export default {
+    name: 'stock',
+    description: 'get stock info',
+    async execute (message, args) {
+        try {
+            const j = await fetch(`https://finnhub.io/api/v1/quote?symbol=${args[0]}&token=br3eovvrh5rai6tgh99g`)
+            const s = await j.json()
+            let ret = `Symbol: ${args[0].toUpperCase()}
+            Current Price: $${s['c']}
+            Today's Open: $${s['o']}
+            Today's Range: $${s['l']} - $${s['h']}
+            Previous Close: $${s['pc']}
+            `
+            return message.channel.send(ret)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
