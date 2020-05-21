bd31da5cbf2249bb3823238d50e0c7f57285a349
feefs <feefs@berkeley.edu>
2020-05-21 16:04:20 -0700

    moved stuff around

--- dm_commands/commands.js
+++ dm_commands/commands.js
@@ -1,9 +1,11 @@
 import verify from './verify.js'
 import email from './email.js'
 import code from './code.js'
+import stock from './stock.js'
 
 export default [
     verify,
     email,
-    code
+    code,
+    stock
 ]
--- reg_commands/commands.js
+++ reg_commands/commands.js
@@ -1,11 +1,9 @@
 import foo from './foo.js'
 import classtorole from './classtorole.js'
 import dadjoke from './dadjoke.js'
-import stock from './stock.js'
 
 export default [
     foo,
     classtorole,
     dadjoke,
-    stock
 ]
