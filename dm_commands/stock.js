06ecbff4faf816951c170ca8d069ecb533e05f79
feefs <feefs@berkeley.edu>
2020-05-21 17:12:59 -0700

    added colors and fixed embed

--- dm_commands/stock.js
+++ dm_commands/stock.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import Discord from 'discord.js'
 export default {
     name: 'stock',
     description: 'get stock info',
@@ -6,14 +7,13 @@ export default {
         try {
             const j = await (await fetch(`https://finnhub.io/api/v1/quote?symbol=${args[0].toUpperCase()}&token=br3eovvrh5rai6tgh99g`)).text()
             const s = JSON.parse(j)
-
             const embed = new Discord.MessageEmbed()
-                .setColor('#fdb515')
+                .setColor(s.c - s.pc > 0 ? '#29bf89' : 'f74356')
                 .setTitle(`Symbol: ${args[0].toUpperCase()}`)
-                .setDescription(`Current Price: $${s['c']}\n` + 
-                    `Today's Open: $${s['o']}\n` + 
-                    `Today's Range: $${s['l']} - $${s['h']}\n` + 
-                    `Previous Close: $${s['pc']}`)
+                .setDescription(`Current Price: $${s.c}\n` + 
+                    `Today's Open: $${s.o}\n` + 
+                    `Today's Range: $${s.l} - $${s.h}\n` + 
+                    `Previous Close: $${s.pc}`)
             return message.channel.send(embed)
         } catch (error) {
             console.error(error)
